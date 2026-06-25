a7788d4103971ec9de994165ea96bdfa5dfe0eb3
Keelan Watlington <kjwat@protonmail.com>
2026-06-25 16:09:09 -0400

    Install PipeWire JACK provider on Arch

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -386,7 +386,7 @@ case "$family" in
         run_package_command arch sudo env LC_ALL=C pacman -S --needed \
             base-devel pkgconf ncurses curl \
             git mpv poppler pandoc-cli \
-            nano zip unzip xdg-utils file less fzf libpulse glib2 wl-clipboard xclip xsel \
+            nano zip unzip xdg-utils file less fzf libpulse pipewire-jack glib2 wl-clipboard xclip xsel \
             isync msmtp calcurse links ca-certificates rsync
         ;;
     alpine)
